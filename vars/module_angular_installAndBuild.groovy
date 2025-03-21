626e91fc336b051e3dbf7d488375342091351baa
Sarathkumarsoundarrajan <sarath.soundar@coherent.in>
2025-03-21 11:36:36 +0530

    Update module_angular_installAndBuild.groovy

--- vars/module_angular_installAndBuild.groovy
+++ vars/module_angular_installAndBuild.groovy
@@ -1,9 +1,17 @@
+#!/usr/bin/env groovy
+
 def call() {
   container('node') {
     sh "echo ${WORKSPACE}"
     sh 'cd ${WORKSPACE}'
     sh 'npm install --force'
-    sh 'npm install -g @angular/cli'
+    def angularCliInstallCommand = "npm install -g "
+    if ("$ANGULAR_VERSION" && "$ANGULAR_VERSION" != "") {
+      angularCliInstallCommand += "$ANGULAR_VERSION"
+    } else {
+      angularCliInstallCommand += "@angular/cli"
+    }
+    sh angularCliInstallCommand
     sh 'ng build'
   }
 }
