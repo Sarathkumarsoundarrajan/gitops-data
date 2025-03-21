1e00ef5b4166eba2c62ef7202bf61f1a035425e5
Sarathkumarsoundarrajan <sarath.soundar@coherent.in>
2025-03-21 13:06:25 +0530

    Update template_angular.groovy

--- vars/template_angular.groovy
+++ vars/template_angular.groovy
@@ -1,65 +1,66 @@
 #!/usr/bin/env groovy
 
+
 def call(
-    String PROJECT_SCM_URL,
-    String PROJECT_NAME,
-    String GITOPS_DATA_PROJECT_PATH,
-    String ANGULAR_VERSION,
-    String NODE_IMAGE,
-    Boolean ENABLE_SONAR,
-    Boolean ENABLE_SONAR_QUALITY_GATE,
-    List<String> SONAR_ENVIRONMENTS
+String PROJECT_SCM_URL,
+String PROJECT_NAME,
+String GITOPS_DATA_PROJECT_PATH,
+String ANGULAR_VERSION,
+String NODE_IMAGE,
+Boolean ENABLE_SONAR,
+Boolean ENABLE_SONAR_QUALITY_GATE,
+List<String> SONAR_ENVIRONMENTS
 ) {
     pipeline {
         agent {
             kubernetes {
                 yaml """
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    app: jenkins-agent
-spec:
-  containers:
-  - name: node
-    image: "${NODE_IMAGE ?: 'node:18-alpine'}"
-    command:
-    - cat
-    tty: true
-  - name: java-node
-    image: timbru31/java-node
-    command:
-    - cat
-    tty: true
-  - name: git
-    image: bitnami/git:latest
-    command:
-    - cat
-    tty: true
-  - name: kaniko
-      image: gcr.io/kaniko-project/executor:latest
-      args: ["--destination=docker.io/kaniko:latest"]
-    command:
-    - cat
-    tty: true
-    volumeMounts:
-    - mountPath: "/workspace"
-      name: "workspace-volume"
-      readOnly: false
-    - name: kaniko-secret
-      mountPath: /kaniko/.docker
-  volumes:
-  - name: kaniko-secret
-    secret:
-      secretName: registry-regcred
-      items:
-        - key: .dockerconfigjson
-          path: config.json
-"""
+        apiVersion: v1
+        kind: Pod
+        metadata:
+          labels:
+            app: jenkins-agent
+        spec:
+          containers:
+          - name: node
+            image: "${NODE_IMAGE ?: 'node:18-alpine'}"
+            command:
+            - cat
+            tty: true
+          - name: java-node
+            image: timbru31/java-node
+            command:
+            - cat
+            tty: true
+          - name: git
+            image: bitnami/git:latest
+            command:
+            - cat
+            tty: true
+          - name: kaniko #
+            image: gcr.io/kaniko-project/executor:debug
+            command:
+            - cat
+            tty: true
+            volumeMounts:
+            - mountPath: "/workspace"
+              name: "workspace-volume"
+              readOnly: false
+            - name: kaniko-secret
+              mountPath: /kaniko/.docker
+          volumes:
+          - name: kaniko-secret
+            secret:
+              secretName: registry-regcred
+              items:
+                - key: .dockerconfigjson
+                  path: config.json
+      """
             }
         }
         environment {
-            IMAGE_NAME = "${COHERENT_DOCKER_REGISTRY_URL}/${PROJECT_NAME.toLowerCase()}:${BUILD_NUMBER}"
+             IMAGE_NAME = "${COHERENT_DOCKER_REGISTRY_URL}/${PROJECT_NAME.toLowerCase()}:${BUILD_NUMBER}"
+            // IMAGE_NAME = "${COHERENT_DOCKER_REGISTRY_URL}/${PROJECT_NAME}:${BUILD_NUMBER}"
             PREVIOUS_BUILD_NUMBER = util_subtractAndConvert("${BUILD_NUMBER}")
             DEPLOYMENT_FILE_PATH = "projects/${GITOPS_DATA_PROJECT_PATH}/k8s-config/${ENVIRONMENT}/deployment.yaml"
             PROJECT_CONFIG_PATH = "gitops-data/projects/${GITOPS_DATA_PROJECT_PATH}/config"
@@ -72,13 +73,14 @@ spec:
         }
 
         stages {
+
             stage('Check out the Source Code') {
                 steps {
                     module_checkOutTheSourceCode()
                 }
             }
 
-            stage('Obtaining Config Files') {
+             stage('Obtaining Config Files') {
                 steps {
                     module_obtainingConfigFiles()
                 }
@@ -106,22 +108,16 @@ spec:
 
             stage('Build Container Image') {
                 steps {
-                    container('kaniko') {  // Correctly reference the kaniko container
-                        sh '''
-                            /kaniko/executor \
-                            --context=${WORKSPACE} \
-                            --dockerfile=${WORKSPACE}/Dockerfile \
-                            --destination=${IMAGE_NAME}
-                        '''
-                    }
+                    module_BuildContainerImage()
                 }
             }
 
             stage('Publish deployment') {
-                steps {
+                steps {                    
                     module_publishDeployment()
                 }
             }
+
         }
     }
 }
