9de5e40f2f31e3e3bd6459c4f6d1a1e7ca4eca24
Sarathkumarsoundarrajan <sarath.soundar@coherent.in>
2025-03-21 11:20:35 +0530

    Update template_react.groovy

--- vars/template_react.groovy
+++ vars/template_react.groovy
@@ -14,40 +14,39 @@ def call(
         kubernetes {
             yaml """
     apiVersion: v1
-    kind: Pod
-    metadata:
-      labels:
-        app: jenkins-agent
-    spec:
-      containers:
-      - name: node
-        image: "${NODE_IMAGE ?: 'node:18-alpine'}"
-        command:
-        - cat
-        tty: true
-      - name: java-node
-        image: timbru31/java-node
-        command:
-        - cat
-        tty: true
-      - name: git
-        image: bitnami/git:latest
-        command:
-        - cat
-        tty: true
-      - name: kaniko
-        image: gcr.io/kaniko-project/executor:latest
-        command:
-        - sleep
-        args:
-        - infinity
-        volumeMounts:
-        - name: kaniko-secret
-          mountPath: /kaniko/.docker
-      volumes:
+kind: Pod
+metadata:
+  labels:
+    app: jenkins-agent
+spec:
+  containers:
+  - name: node
+    image: "node:18-alpine"
+    command:
+    - cat
+    tty: true
+  - name: java-node
+    image: timbru31/java-node
+    command:
+    - cat
+    tty: true
+  - name: git
+    image: bitnami/git:latest
+    command:
+    - cat
+    tty: true
+  - name: kaniko   # <-- Add this
+    image: gcr.io/kaniko-project/executor:latest
+    command:
+    - cat
+    tty: true
+    volumeMounts:
       - name: kaniko-secret
-        secret:
-          secretName: docker-config
+        mountPath: /kaniko/.docker
+  volumes:
+    - name: kaniko-secret
+      secret:
+        secretName: docker-config
   """
         }
     }
