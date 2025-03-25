65d15c935c0cd3c55e3eb88182980f15d0361d20
Sarathkumarsoundarrajan <sarath.soundar@coherent.in>
2025-03-25 10:39:09 +0530

    Update template_angular.groovy

--- vars/template_angular.groovy
+++ vars/template_angular.groovy
@@ -10,102 +10,116 @@ def call(
     Boolean ENABLE_SONAR_QUALITY_GATE,
     List<String> SONAR_ENVIRONMENTS
 ) {
-    pipeline {
-        agent {
-            kubernetes {
-                yaml """
-        apiVersion: v1
-        kind: Pod
-        metadata:
-          labels:
-            app: jenkins-agent
-        spec:
-          containers:
-          - name: node
-            image: "${NODE_IMAGE ?: 'node:18-alpine'}"
-            command: ["cat"]
-            tty: true
-          - name: java-node
-            image: timbru31/java-node
-            command: ["cat"]
-            tty: true
-          - name: git
-            image: bitnami/git:latest
-            command: ["cat"]
-            tty: true
-          - name: kaniko
-            image: gcr.io/kaniko-project/executor:debug
-            command: ["cat"]
-            tty: true
-            volumeMounts:
-            - mountPath: "/workspace"
-              name: "workspace-volume"
-              readOnly: false
-            - name: kaniko-secret
-              mountPath: /kaniko/.docker
-          volumes:
-          - name: kaniko-secret
-            secret:
-              secretName: registry-regcred
-              items:
-                - key: .dockerconfigjson
-                  path: config.json
-      """
-            }
-        }
-
-        environment {
-            IMAGE_NAME = "${COHERENT_DOCKER_REGISTRY_URL}/${PROJECT_NAME.toLowerCase()}:${BUILD_NUMBER}"
-            PREVIOUS_BUILD_NUMBER = util_subtractAndConvert("${BUILD_NUMBER}")
-            DEPLOYMENT_FILE_PATH = "projects/${GITOPS_DATA_PROJECT_PATH}/k8s-config/${ENVIRONMENT}/deployment.yaml"
-            PROJECT_CONFIG_PATH = "gitops-data/projects/${GITOPS_DATA_PROJECT_PATH}/config"
-            COMMON_PATH = "gitops-data/common"
-            SONAR_TOKEN = credentials('SONAR_TOKEN')
-            COHERENT_GITOPS_DATA_TOKEN = credentials('COHERENT_GITOPS_DATA_TOKEN')
-        }
-
-        stages {
-            stage('Check out the Source Code') {
-                steps {
-                    module_checkOutTheSourceCode()
+    return {
+        pipeline {
+            agent {
+                kubernetes {
+                    yaml """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              labels:
+                app: jenkins-agent
+            spec:
+              containers:
+              - name: node
+                image: "${NODE_IMAGE}"
+                command: ["cat"]
+                tty: true
+              - name: java-node
+                image: timbru31/java-node
+                command: ["cat"]
+                tty: true
+              - name: git
+                image: bitnami/git:latest
+                command: ["cat"]
+                tty: true
+              - name: kaniko
+                image: gcr.io/kaniko-project/executor:debug
+                command: ["cat"]
+                tty: true
+                volumeMounts:
+                - mountPath: "/workspace"
+                  name: "workspace-volume"
+                  readOnly: false
+                - name: kaniko-secret
+                  mountPath: /kaniko/.docker
+              volumes:
+              - name: kaniko-secret
+                secret:
+                  secretName: registry-regcred
+                  items:
+                    - key: .dockerconfigjson
+                      path: config.json
+          """
                 }
             }
 
-            stage('Obtaining Config Files') {
-                steps {
-                    module_obtainingConfigFiles()
-                }
+            environment {
+                IMAGE_NAME = "${COHERENT_DOCKER_REGISTRY_URL}/${PROJECT_NAME.toLowerCase()}:${BUILD_NUMBER}"
+                PREVIOUS_BUILD_NUMBER = util_subtractAndConvert("${BUILD_NUMBER}")
+                DEPLOYMENT_FILE_PATH = "projects/${GITOPS_DATA_PROJECT_PATH}/k8s-config/${ENVIRONMENT}/deployment.yaml"
+                PROJECT_CONFIG_PATH = "gitops-data/projects/${GITOPS_DATA_PROJECT_PATH}/config"
+                COMMON_PATH = "gitops-data/common"
+                SONAR_TOKEN = credentials('SONAR_TOKEN')
+                COHERENT_GITOPS_DATA_TOKEN = credentials('COHERENT_GITOPS_DATA_TOKEN')
             }
 
-            stage('SonarQube Analysis') {
-                when { expression { ENABLE_SONAR && util_isStringInList(ENVIRONMENT, SONAR_ENVIRONMENTS) } }
-                steps {
-                    module_js_sonarQubeAnalysis()
+            stages {
+                stage('Check out the Source Code') {
+                    steps {
+                        script {
+                            module_checkOutTheSourceCode()
+                        }
+                    }
                 }
-            }
 
-            stage('Quality Gate') {
-                when { expression { ENABLE_SONAR_QUALITY_GATE && ENABLE_SONAR && util_isStringInList(ENVIRONMENT, SONAR_ENVIRONMENTS) } }
-                steps {
-                    waitForQualityGate abortPipeline: true
+                stage('Obtaining Config Files') {
+                    steps {
+                        script {
+                            module_obtainingConfigFiles()
+                        }
+                    }
                 }
-            }
 
-            stage('Install and Build') {
-                steps {
-                    module_angular_installAndBuild()
+                stage('SonarQube Analysis') {
+                    when { expression { ENABLE_SONAR && util_isStringInList(ENVIRONMENT, SONAR_ENVIRONMENTS) } }
+                    steps {
+                        script {
+                            module_js_sonarQubeAnalysis()
+                        }
+                    }
                 }
-            }
 
-            stage('Build Container Image') {
-                steps {
-                    module_BuildContainerImage()
+                stage('Quality Gate') {
+                    when { expression { ENABLE_SONAR_QUALITY_GATE && ENABLE_SONAR && util_isStringInList(ENVIRONMENT, SONAR_ENVIRONMENTS) } }
+                    steps {
+                        waitForQualityGate abortPipeline: true
+                    }
                 }
-            }
 
-            stage('Publish Deployment') {
-                steps {
-                    module_publishDeployment()
+                stage('Install and Build') {
+                    steps {
+                        script {
+                            module_angular_installAndBuild()
+                        }
+                    }
+                }
+
+                stage('Build Container Image') {
+                    steps {
+                        script {
+                            module_BuildContainerImage()
+                        }
+                    }
+                }
+
+                stage('Publish Deployment') {
+                    steps {
+                        script {
+                            module_publishDeployment()
+                        }
+                    }
                 }
             }
         }
